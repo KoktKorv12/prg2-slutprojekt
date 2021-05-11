25c8bcbc9583070571161ed6461a98fdafa5d93f
carl.berglund <redguyshrug@gmail.com>
2021-05-11 13:35:51 +0200

    form

---
+++ src/databaseLogin.java
@@ -0,0 +1,7 @@
+public class databaseLogin {
+        static String user = "calleflunk";
+        static String password = "Nummer24";
+        static String port = "3306";
+        static String DBname = "fredag";
+        static String DBURL = "localhost";
+}
